634d754ca9ef35fdf5d45fcfc0f2eb79469ee455
Alexandr <pioneer1593@mail.ru>
2017-09-04 07:01:15 +0300

    Commit message

--- unogamebot/src/main/java/bots/unogamebot/App.java
+++ unogamebot/src/main/java/bots/unogamebot/App.java
@@ -6,10 +6,7 @@ package bots.unogamebot;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
     }
 }
